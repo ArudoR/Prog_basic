3e963b73ea95f8fdbbb7f738cf86e7bd45c88dfa
Gerardo Mejia <aldonear@gmail.com>
2023-03-24 12:46:45 -0600

    movimiento con 4 botones usando velocidad

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -44,6 +44,7 @@ let vidasJugador = 3;
 let vidasEnemigo = 3;
 
 let lienzo = mapa.getContext("2d")
+let intervalo
 
 class Mokepon {
     constructor(nombre, foto, vida){
@@ -57,6 +58,8 @@ class Mokepon {
         this.alto = 80
         this.mapaFoto = new Image()
         this.mapaFoto.src = foto
+        this.velocidadX = 0
+        this.velocidadY = 0
     }
 }
 
@@ -117,6 +120,7 @@ function seleccionarMascotaJugador(){
     sectionSeleccionarMascota.style.display = 'none';
     //sectionSeleccionarAtaque.style.display = 'flex';
     sectionVerMapa.style.display = 'flex'
+    intervalo = setInterval(pintarPersonaje, 50)
     
 
     if(inputHipodogue.checked){
@@ -277,6 +281,9 @@ function aleatorio(min, max){
 }
 
 function pintarPersonaje(){
+    capipepo.x = capipepo.x + capipepo.velocidadX
+    capipepo.y =  capipepo.y + capipepo.velocidadY
+
     lienzo.clearRect(0, 0, mapa.width, mapa.height)
     lienzo.drawImage(
         capipepo.mapaFoto,
@@ -287,9 +294,25 @@ function pintarPersonaje(){
     )
 }
 
-function moverCapipepo(){
-    capipepo.x = capipepo.x + 5
-    pintarPersonaje()
+function moverDerecha(){
+    capipepo.velocidadX = 5
+}
+
+function moverIzquierda(){
+    capipepo.velocidadX = -5
+}
+
+function moverAbajo(){
+    capipepo.velocidadY = 5 
+}
+
+function moverArriba(){
+    capipepo.velocidadY = -5
+}
+
+function detenerMovimiento(){
+    capipepo.velocidadX = 0
+    capipepo.velocidadY = 0
 }
 
 window.addEventListener('load', iniciarJuego);
