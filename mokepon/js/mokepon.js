aba7ac94c1a664c9d5737918affef3ac80ff5828
Gerardo Mejia <aldonear@gmail.com>
2023-04-12 13:57:12 -0600

    comunicación de frontend con backend con un API

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -167,6 +167,20 @@ function iniciarJuego(){
     sectionReiniciar.style.display = 'none';
     botonMascota.addEventListener('click', seleccionarMascotaJugador);
     botonReiniciar.addEventListener('click', reiniciarJuego);
+
+    unirseAlJuego()
+}
+
+function unirseAlJuego(){
+    fetch("http://localhost:8080/unirse")
+        .then(function(res){
+            if (res.ok) {
+                res.text()
+                    .then(function (respuesta){
+                        console.log(respuesta)
+                    })
+            }
+        })
 }
 
 function seleccionarMascotaJugador(){
