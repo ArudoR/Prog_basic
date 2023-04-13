6864e158ea828a160b3d2cb13dcfe09181bb7b7b
Gerardo Mejia <aldonear@gmail.com>
2023-04-12 19:07:59 -0600

    JSON y POST

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -21,6 +21,7 @@ const contenedorAtaques = document.getElementById('contenedorAtaques')
 const sectionVerMapa = document.getElementById('ver-mapa')
 const mapa = document.getElementById('mapa')
 
+let jugadorId = null
 let mokepones = []
 let ataqueJugador = []
 let ataqueEnemigo = []
@@ -178,6 +179,7 @@ function unirseAlJuego(){
                 res.text()
                     .then(function (respuesta){
                         console.log(respuesta)
+                        jugadorId = respuesta
                     })
             }
         })
@@ -200,9 +202,23 @@ function seleccionarMascotaJugador(){
         reiniciarJuego();
     }
 
+    seleccionarMokepon(mascotaJugador)
+
     extraerAtaques(mascotaJugador);
     sectionVerMapa.style.display = 'flex'
     iniciarMapa()
+}
+
+function seleccionarMokepon(mascotaJugador){
+    fetch(`http://localhost:8080/mokepon/${jugadorId}`, {
+    method: "post",
+    headers: {
+    "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+    mokepon: mascotaJugador
+    })
+    })
 }
 
 function extraerAtaques(mascotaJugador){
